4bd61301392e49209e1d6101f483d50bd9bbda7d
Ozan Caglayan <ozancag@gmail.com>
2018-01-17 21:45:45 +0100

    scripts: small fix for python compat

--- scripts/task1-tok-stats.sh
+++ scripts/task1-tok-stats.sh
@@ -10,7 +10,7 @@ for split in train val test_2016_flickr test_2017_flickr test_2017_mscoco; do
     WC=`cat ${split}.lc.norm.tok.${lang} | wc`
     N_SENTS=`echo $WC | cut -d' ' -f1`
     N_WORDS=`echo $WC | cut -d' ' -f2`
-    N_WORD_PER_SENT=`python -c "print('%.1f' % (${N_WORDS} / ${N_SENTS}))"`
+    N_WORD_PER_SENT=`python3 -c "print('%.1f' % (${N_WORDS} / ${N_SENTS}))"`
     echo " ($lang) $N_SENTS sentences, $N_WORDS words, $N_WORD_PER_SENT words/sent"
   done
 done
